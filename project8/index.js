1bae5afd1649d04e9fda69310af5cf7781f76342
DipNarayanMahara2 <dipnarayanmahara28@gmail.com>
2024-12-18 09:07:42 +0545

    almost completed project 8

---
+++ project8/index.js
@@ -0,0 +1,13 @@
+// welcome alert
+
+alert("welcome ! Users");
+
+// Function to check if the user is already accepted cookes
+
+// function to show cookies popup
+
+// Function to hide cookies pop Up and save the content
+
+// addnig event listener when the user accept cookies
+
+// check if user already accept cookies
